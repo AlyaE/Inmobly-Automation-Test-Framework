78be792eeca84af50b2ed189f5e6f23f8359f4ac
AlyaE <AlyaElGamal@gmail.com>
2021-11-06 17:39:00 -0400

    Started adding Cucumber and Gherkin

---
+++ practice2/src/StepDefinition/Self.java
@@ -0,0 +1,12 @@
+package StepDefinition;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Self {
+	ChromeDriver driver;
+	
+	public ChromeDriver Self() {
+		return driver;
+	}
+}
---
+++ practice2/src/StepDefinition/Steps.java
@@ -0,0 +1,64 @@
+package StepDefinition;
+
+import static org.testng.Assert.assertEquals;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import Pages.DashboardLogin;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import UseCaseTests.DasboardLoginTest;
+
+public class Steps {
+	private Self self;
+	
+	public Steps(Self self) {
+		this.self = self;
+	}
+	
+	@Before("@launchDashboard")
+	public void i_launch_the_dashboard_in_chrome() {
+		self.driver = new ChromeDriver();
+		self.driver.manage().window().maximize();
+		self.driver.get("https://ott.bolt-play.com/login");
+	    throw new io.cucumber.java.PendingException();
+	}
+	
+	@When("I am logged in as {string} with the password {string}")
+	public void aNewUser(String user, String password, Self self) {
+		System.out.println("beginning of func");
+		self.driver.findElement(By.xpath(DashboardLogin.textbox_uername)).click();
+		self.driver.findElement(By.xpath(DashboardLogin.textbox_uername)).sendKeys("inmobly-admin");
+		self.driver.findElement(By.xpath(DashboardLogin.testbox_password)).click();
+		self.driver.findElement(By.xpath(DashboardLogin.testbox_password)).sendKeys("support123456");
+		self.driver.findElement(By.xpath(DashboardLogin.button_signin)).click();
+		System.out.println("before assert");
+		self.driver.manage().timeouts().implicitlyWait(3000, TimeUnit.SECONDS);
+		String msg = self.driver.findElement(By.xpath(DashboardLogin.button_inmobly_app)).getText();
+		assertEquals(msg, "Inmobly App");
+        
+    }
+        
+
+	
+    @When("^Enter the Username and Password$")
+    public void enter_the_Username_and_Password() throws Throwable
+    {
+       System.out.println("This step enter the Username and Password on the login page.");
+    }
+
+    @Then("^Reset the credential$")
+    public void Reset_the_credential() throws Throwable 							
+    {
+        System.out.println("This step click on the Reset button.");
+    }
+
+}
